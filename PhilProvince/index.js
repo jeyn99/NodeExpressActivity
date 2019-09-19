fcebbb509c88b251b021c2ff0cfe9b04e0c7b98e
repolloja_sd2023@CEACCS.NET <repolloja_sd2023@CEACCS.NET>
2019-09-19 16:00:56 +0800

    This is to be edit

--- PhilProvince/index.js
+++ PhilProvince/index.js
@@ -1,10 +1,40 @@
-const express = require('express');
-const app = express();
+var app = require('express')();
+var express = require('express');
+var http = require("http").Server(app)
+var port = process.env.PORT || 1234;
+var path = require('path');
+var fs = require('fs')
 app.set('view engine', 'pug');
 app.set('views','views');
 
-
-app.get('/index', function (req, res) {
-  res.render('sample')
+app.get('/', function (req, res) {
+  res.end("Hello")
 })
-app.listen(8081);
+
+app.use(express.static(path.join(__dirname, '/public')))
+
+
+app.get('/province/:name', function (req, res) {
+  var provData;
+  var name = req.params.name + ".json"
+  fs.readFile("./province/" + name, function (err, data) {
+    provData = JSON.parse(data);
+    console.log(provData.name);
+    res.render("sample", { 
+      img1: provData.images[0], 
+      img2: provData.images[1],
+      img3: provData.images[2],
+      name: provData.name, 
+      group: provData.group, 
+      population: provData.population,
+      delicacies: provData.delicacies,
+      writeup: provData.writeUp
+    })
+    
+  })
+})
+
+
+http.listen(port, function () {
+  console.log('listening on *:' + port);
+});
