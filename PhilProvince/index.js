acde815a4718f35daf41600edb41fbe935d1d5ef
2ndyrGroupC <2ndyrGroupC@pn.local>
2019-09-22 15:46:38 +0800

    This is to be edit and check

--- PhilProvince/index.js
+++ PhilProvince/index.js
@@ -6,6 +6,7 @@ var path = require('path');
 var fs = require('fs')
 app.set('view engine', 'pug');
 app.set('views','views');
+var proVisit = {"bohol":0, "cebu": 0, "negros-oriental": 0, "camiguin": 0, "pangasinan": 0}
 
 app.get('/', function (req, res) {
   res.end("Hello")
@@ -16,11 +17,13 @@ app.use(express.static(path.join(__dirname, '/public')))
 
 app.get('/province/:name', function (req, res) {
   var provData;
-  var name = req.params.name + ".json"
-  fs.readFile("./province/" + name, function (err, data) {
+  var name = req.params.name;
+  proVisit[name]++;
+  console.log(proVisit);
+  fs.readFile("./province/" + name + ".json", function (err, data) {
     provData = JSON.parse(data);
-    console.log(provData.name);
     res.render("sample", { 
+      shortname:provData.shortname,
       img1: provData.images[0], 
       img2: provData.images[1],
       img3: provData.images[2],
@@ -28,9 +31,9 @@ app.get('/province/:name', function (req, res) {
       group: provData.group, 
       population: provData.population,
       delicacies: provData.delicacies,
-      writeup: provData.writeUp
+      writeup: provData.writeUp,
+      rating: provData.rating
     })
-    
   })
 })
 
