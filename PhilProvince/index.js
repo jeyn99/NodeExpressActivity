b199fc95f04171b3771818ae617961bff3a8f327
1styrGroupC <1styrGroupC@pn.local>
2019-09-24 09:01:09 +0800

    This is amazing to edit later

--- PhilProvince/index.js
+++ PhilProvince/index.js
@@ -3,31 +3,40 @@ var express = require('express');
 var http = require("http").Server(app)
 var port = process.env.PORT || 1234;
 var path = require('path');
+var count = require('./public/controller')
 var fs = require('fs')
 app.set('view engine', 'pug');
-app.set('views','views');
-var proVisit = {"bohol":0, "cebu": 0, "negros-oriental": 0, "camiguin": 0, "pangasinan": 0}
-let pageVisit = 0;
+app.set('views', 'views');
+var proVisit = { "bohol": 0, "cebu": 0, "negros-oriental": 0, "camiguin": 0, "pangasinan": 0 }
+// var count;
+
 app.get('/', function (req, res) {
+  //count.counter(req)
   res.end("Hello")
 })
 
 app.use(express.static(path.join(__dirname, '/public')))
 
+app.use((req, res, next) => {
+  //console.log(`${req.method} ${req.originalUrl}`)
+  count.counter(req);
+  next();
+})
+
 app.get('/province/:name', function (req, res) {
   var provData;
   var name = req.params.name;
-  proVisit[name]++;
+  proVisit[name]*1+1;
   console.log(proVisit);
   fs.readFile("./province/" + name + ".json", function (err, data) {
     provData = JSON.parse(data);
-    res.render("sample", { 
-      shortname:provData.shortname,
-      img1: provData.images[0], 
+    res.render("index", {
+      shortname: provData.shortname,
+      img1: provData.images[0],
       img2: provData.images[1],
       img3: provData.images[2],
-      name: provData.name, 
-      group: provData.group, 
+      name: provData.name,
+      group: provData.group,
       population: provData.population,
       delicacies: provData.delicacies,
       writeup: provData.writeUp,
@@ -36,7 +45,11 @@ app.get('/province/:name', function (req, res) {
   })
 })
 
-
+app.get("*", function (req, res) {
+  res.send("Not found");
+  res.end();
+  count.counter(req)
+});
 
 http.listen(port, function () {
   console.log('listening on *:' + port);
--- PhilProvince/public/controller.js
+++ PhilProvince/public/controller.js
@@ -1,6 +1,13 @@
 var fs = require('fs')
 
-
-$(".one").click(function() {
-    
-})
+exports.counter = function(req){
+    let count;
+    fs.readFile('num-request.txt',"utf-8", function (err, data) {
+        //console.log(data+1)*1;
+        count = (data * 1)+1;
+        fs.writeFile('num-request.txt', count, function (err) {
+          if (err) throw err;
+          console.log("Saved!")
+        });
+      });
+}
